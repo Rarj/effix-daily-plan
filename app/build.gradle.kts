7180fe668661c34a3bedf2587805c2b9d374f025
Rio Arjuna <developer.arj@gmail.com>
2025-04-26 13:53:42 +0700

    initial commit
    Signed-off-by: Rio Arjuna <developer.arj@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,14 @@ plugins {
 
 android {
     namespace = "com.arj.effix"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.arj.effix"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
